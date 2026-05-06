3c8088a33bd493f78980c22645f5a123ed12acd5
Syed Bilal Alam <107833932+syedbilalalam@users.noreply.github.com>
2026-05-06 06:24:37 +0500

    feat: lab 6 added
    Co-authored-by: Smh Code <205378024+syedbilaldev@users.noreply.github.com>

---
+++ Lab_6/Pyramid Pattern/src/pyramid/pattern/PyramidPattern.java
@@ -0,0 +1,18 @@
+package pyramid.pattern;
+
+public class PyramidPattern {
+    public static void main(String[] args) {
+        int rows = 5;
+        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 0; j < rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
